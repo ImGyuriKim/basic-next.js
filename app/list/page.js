1fbdfed4cafde82c249fb924246ce0bfb389163c
ImGyuriKim <gyurikim0105@gmail.com>
2023-06-02 14:40:16 +0900

    feat add image

--- app/list/page.js
+++ app/list/page.js
@@ -1,24 +1,39 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-// import Link from "next/link";
 
 function List() {
   const router = useRouter();
+  let 상품 = ["tomato", "pasta", "coconut"];
+
   return (
     <div>
       <h1 className="title">상품 목록</h1>
 
-      <p
+      {/* <div
         className="product"
         onClick={() => {
           router.push("/list/product_1");
         }}
       >
-        상품 1 $40
-      </p>
+        {상품[0]} 40$
+      </div>
 
-      <p className="product">상품 2 $50</p>
+      <div className="product">{상품[1]} $50</div>
+      <div className="product">{상품[2]} $50</div> */}
+
+      {상품.map((el, i) => {
+        return (
+          <div key={i} className="product">
+            <img
+              src={`/${el}.jpeg`}
+              alt={`${el}`}
+              className="product-img"
+            ></img>
+            {el} $50
+          </div>
+        );
+      })}
     </div>
   );
 }
